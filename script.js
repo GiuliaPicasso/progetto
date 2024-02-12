b4db9d765b05d8451b083144704536405e4a29cf
GiuliaPicasso <giulia.picasso@mail.polimi.it>
2024-02-12 19:09:42 +0100

    x

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ window.transitionToPage = function(href) {
 document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
+
+function openNav(navId) {
+    document.getElementById(navId).style.width = "100%";
+}
+
+function closeNav(navId) {
+    document.getElementById(navId).style.width = "0%";
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}

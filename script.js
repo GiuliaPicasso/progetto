0a10b17b7213772d786fd83bd47ac34961e9e665
GiuliaPicasso <giulia.picasso@mail.polimi.it>
2024-02-18 15:03:51 +0100

    jj

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.transitionToPage = function(href) {
     document.querySelector('body').style.opacity = 0
     setTimeout(function() {
         window.location.href = href
-    }, 300)
+    }, 100)
 }
 
 
